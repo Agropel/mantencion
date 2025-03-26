5dce50d3abd72af2e30ded3c40827dfa2f62c6d4
mantencionagropel <agropelmantencion@gmail.com>
2025-03-26 11:40:19 -0300

    first commit55

--- app/admin/maquinas/[id]/edit/page.tsx
+++ app/admin/maquinas/[id]/edit/page.tsx
@@ -1,11 +1,12 @@
 import { NextPage } from "next";
-import EditMaquinasForm from "@/components/maquinas/EditMaquinasForm"
-import MaquinasForm from "@/components/maquinas/MaquinasForm"
-import GoBackButton from "@/components/ui/GoBackButton"
-import Heading from "@/components/ui/Heading"
-import { prisma } from "@/src/lib/prisma"
-import { notFound } from "next/navigation"
+import EditMaquinasForm from "@/components/maquinas/EditMaquinasForm";
+import MaquinasForm from "@/components/maquinas/MaquinasForm";
+import GoBackButton from "@/components/ui/GoBackButton";
+import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
+import { notFound } from "next/navigation";
 
+// Función para obtener la máquina por ID de forma asíncrona
 async function getMaquinaById(id: number) {
     if (isNaN(id)) {
         notFound(); // Manejo de error si el ID no es válido
@@ -24,8 +25,10 @@ async function getMaquinaById(id: number) {
 
 // Definimos el tipo de página con NextPage
 const EditMaquinasPage: NextPage<{ params: { id: string } }> = async ({ params }) => {
+    // Asegúrate de convertir el id de string a número correctamente
     const idNumber = Number(params.id);
 
+    // Obtener la máquina a través de la función asíncrona
     const maquina = await getMaquinaById(idNumber);
 
     return (
